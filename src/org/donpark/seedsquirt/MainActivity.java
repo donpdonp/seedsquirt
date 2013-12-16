c1cecb1651abc2fcc625c2f637d78b3830b8a703
Don Park <donp@donp.org>
2013-12-15 18:31:19 -0800

    start the activity

--- src/org/donpark/seedsquirt/MainActivity.java
+++ src/org/donpark/seedsquirt/MainActivity.java
@@ -2,9 +2,11 @@ package org.donpark.seedsquirt;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements Constants {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +14,14 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d(APP_TAG, "App starting. Starting service");
+        Intent watch = new Intent(this, WatchService.class);
+        startService(watch);
+    }
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
